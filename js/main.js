c7bc9a600f0523a519b5ff9ea20f1d9ec105037c
inkopro <55760539+inkopro@users.noreply.github.com>
2024-07-03 17:56:52 +0500

    fix

--- js/main.js
+++ js/main.js
@@ -2,8 +2,9 @@ const menu = document.querySelector('.header__mobile__mnu')
 const menuBtn = document.querySelector('.header__burger-btn')
 const menuOverlay = document.querySelector('.overlay')
 const menuClose = document.querySelector('.header__close-btn')
+const menuLink = document.querySelector('.header__mnu__item')
 const body = document.body;
-
+// кнопка бургера
 if (menu && menuBtn) {
   menuBtn.addEventListener('click', () => {
     menu.classList.toggle('active')
@@ -11,11 +12,36 @@ if (menu && menuBtn) {
     menuOverlay.classList.toggle('active')
     body.classList.toggle('lock')
   })
+  // /кнопка бурегера
 
+  // кнопка закрытия мобильного меню
   menuClose.addEventListener('click', () => {
     menu.classList.remove('active')
     menuOverlay.classList.remove('active')
     body.classList.remove('lock')
   })
+  menuLink.addEventListener('click', () => {
+    menu.classList.remove('active')
+    menuOverlay.classList.remove('active')
+    body.classList.remove('lock')
+  })
 
+  // /кнопка закрытия мобильного меню
 }
+
+// плавный скролл
+const anchors = document.querySelectorAll('a[href*="#"]');
+
+anchors.forEach(anchors => {
+  anchors.addEventListener('click', event => {
+    event.preventDefault();
+
+    const blockID = anchors.getAttribute('href').substring(1);
+
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  })
+})
+// /плавный скролл
